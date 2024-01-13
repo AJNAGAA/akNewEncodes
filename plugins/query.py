cfc0e459c47326dab3e66f8fa7cae47286891121
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-13 09:06:22 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -119,7 +119,7 @@ async def Cb_Handle(bot: Client, query: CallbackQuery):
         except Exception as e:
             print(e)
 
-      elif data == '540pc':
+    elif data == '540pc':
         try:
             c_thumb = await db.get_thumbnail(query.from_user.id)
             ffmpeg = "-preset faster -c:v libx265 -s 960x540 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"
