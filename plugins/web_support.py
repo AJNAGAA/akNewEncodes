f80e9793567700377539f6b103835890aae3ebd1
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-01-13 09:35:34 +0530

    Update web_support.py

--- plugins/web_support.py
+++ plugins/web_support.py
@@ -4,7 +4,7 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("WOODEncoderBot")
+    return web.json_response("HarshaEncoderBot")
 
 
 async def web_server():
